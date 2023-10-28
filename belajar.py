cc0ced87dad107b3ba42601553c37d099166595b
karan royka <karanroyka27@gmail.com>
2023-10-28 08:16:20 +0700

    first Commit

---
+++ belajar.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+import getpass
+
+#konstanta password
+PASSWORD = "1234"
+
+# pembersih 
+def bersih():
+    os.system("clear")
+
+# tampilan 
+def menu():
+    bersih()
+    os,sistem("figlet Semangat Belajar")
