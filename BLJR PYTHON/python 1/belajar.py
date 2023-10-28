1520d1e218e4842ee1cd78841fff3a03e4e22da9
karan royka <88747204+12kaj@users.noreply.github.com>
2023-10-28 09:17:58 +0700

    Delete BLJR PYTHON directory

--- BLJR PYTHON/python 1/belajar.py
+++
@@ -1,1 +0,0 @@
-print("saya adalah karan")
--- BLJR PYTHON/python/kirimpesan.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import pywhatkit
-import time
-
-pywhatkit.sendwhatmsg("+6282283024761","ngetes",0,1)
-time.sleep(1)
-pyautogui.click()
-time.sleep(1)
-pyautogui.press('enter')
